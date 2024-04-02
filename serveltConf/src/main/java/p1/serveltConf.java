c5c0de5d8dd53e83c8d31fd15a5c9aac0583bb14
pune24 <165369038+pune24@users.noreply.github.com>
2024-04-02 22:41:13 +0530

    Add files via upload

---
+++ serveltConf/src/main/java/p1/serveltConf.java
@@ -0,0 +1,39 @@
+package p1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class serveltConf
+ */
+//@WebServlet("/serveltConf")
+public class serveltConf extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public serveltConf() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		 response.setContentType("text/html");
+		 PrintWriter out=response.getWriter();
+		 
+		ServletConfig con=getServletConfig();
+		 out.print(""+con.getInitParameter("driver"));
+		 
+
+		 
+		 
+	}
+
+}
